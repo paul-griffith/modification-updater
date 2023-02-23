c0361dc5d6b388c617efeb8797cbeb3859a99d3e
Brad <bradley.phillips@bhp.com>
2023-02-23 19:37:04 +0800

    feat: replaces resource file by default
    the most 'typical' usecase is to update the resource.json file in-place,
    as a 'one-liner'
    
    consequentially, more command line options were added, "no-replace" and
    "console"
    
    this way, the user can still use the previous behaviour if required, but
    with no extra arguments
    
    a default value was added for time also
    
    the tests were tweaked to work with the new arguments, and stdout
    behaviour must be different between machines, because newline characters
    were causing tests to fail, this has been guarded against agressively.

--- src/main/kotlin/io/github/paulgriffith/modification/Entrypoint.kt
+++ src/main/kotlin/io/github/paulgriffith/modification/Entrypoint.kt
@@ -10,40 +10,122 @@ import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.path
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.encodeToStream
+import java.io.FileOutputStream
 import java.nio.file.Path
 import java.time.Instant
 import kotlin.io.path.inputStream
 import kotlin.io.path.readBytes
 
-class ModificationUpdater : CliktCommand(help = "Pass path(s) to resources to update their attributes accordingly.", printHelpOnEmptyArgs = true) {
-    private val signature: Boolean by option("-s", "--signature").flag()
-    private val resources: List<Path> by argument(help = "The file to target").path(
+class ModificationUpdater : CliktCommand(
+    help = "Pass path(s) to resources to update their attributes accordingly.",
+    printHelpOnEmptyArgs = true
+) {
+    private val sigOnly: Boolean by option(
+        "-s", "--signature",
+        help="Prints the signature only"
+    ).flag()
+
+    private val noReplace: Boolean by option(
+        "-nr", "--no-replace",
+        help="Will NOT replace the resource.json file"
+    ).flag()
+
+    private val toConsole: Boolean by option(
+        "-c", "--console",
+        help="Prints the new file to the console"
+    ).flag()
+
+    private val resources: List<Path> by argument(
+        help = "The file to target"
+    ).path(
         mustExist = true,
         canBeFile = false,
         mustBeReadable = true
     ).multiple()
-    private val actor by option("-a", "--actor", help = "The new actor name").default("external")
-    private val timestamp by option("-t", "--timestamp", help = "The update timestamp").convert { Instant.parse(it) }
+
+    private val actor by option(
+        "-a",
+        "--actor",
+        help = "The new actor name"
+    ).default("external")
+
+    private val timestamp by option(
+        "-t",
+        "--timestamp",
+        help = "The update timestamp"
+    ).convert {
+        Instant.parse(it)
+    }.default(
+        Instant.now()
+    )
 
     override fun run() {
         resources.forEach { resourcePath ->
-            val resource = getResource(resourcePath)
-            if (signature) {
+            val resourceFile = locateResourceFile(resourcePath)
+            var manifest = resourceToManifest(resourceFile)
+            val resource = getProjectResource(resourcePath, manifest)
+
+            if (sigOnly) {
                 println(resource.getSignature())
-            } else {
-                val updated = resource.update(actor, timestamp)
-                println(JSON.encodeToString(ResourceManifest.serializer(), updated.manifest))
+            }
+
+            var updated: ProjectResource? = null
+            if (noReplace || toConsole){
+                updated = resource.update(actor, timestamp)
+            }
+
+            if (updated != null && toConsole){
+                println(
+                    JSON.encodeToString(
+                        ResourceManifest.serializer(),
+                        updated.manifest
+                    )
+                )
+            }
+
+            if (updated != null && !noReplace){
+                writeManifest(
+                    resourceFile,
+                    updated.manifest
+                )
             }
         }
     }
 
-    private fun getResource(location: Path): ProjectResource {
-        val manifest = location.resolve("resource.json").inputStream().toManifest()
-        val data = manifest.files.associateWith { fileName ->
-            DataLoader { location.resolve(fileName).readBytes() }
-        }
-        return ProjectResource(manifest, data)
+    private fun locateResourceFile(folderPath: Path) : Path{
+        return folderPath.resolve("resource.json");
     }
+
+    private fun resourceToManifest(resourceFile: Path): ResourceManifest{
+        return resourceFile.inputStream().toManifest()
+    }
+
+    private fun getProjectResource(
+        folderPath: Path,
+        manifest: ResourceManifest
+    ) : ProjectResource{
+        return ProjectResource(
+            manifest,
+            manifest.files.associateWith {
+                    fn -> DataLoader { folderPath.resolve(fn).readBytes() }
+            }
+        )
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    private fun writeManifest(
+        resourceFile: Path,
+        manifest: ResourceManifest
+    ){
+        JSON.encodeToStream(
+            ResourceManifest.serializer(),
+            manifest,
+            FileOutputStream(resourceFile.toFile())
+        )
+    }
+
 }
 
 fun main(args: Array<String>) = ModificationUpdater().main(args)
--- src/test/kotlin/io/github/paulgriffith/modification/CliTests.kt
+++ src/test/kotlin/io/github/paulgriffith/modification/CliTests.kt
@@ -17,24 +17,52 @@ class CliTests : FunSpec({
 
     test("Signature check") {
         captureStandardOut {
-            cli.parse(listOf("-s", "src/test/resources/script"))
-        } shouldBe "7ea951abc0ddc97f549f41a5670b06aa513b30e189050159f40e207cfe502b02\n"
+            cli.parse(listOf("-s", "-nr", "src/test/resources/script"))
+        }.trim() shouldBe "7ea951abc0ddc97f549f41a5670b06aa513b30e189050159f40e207cfe502b02"
     }
 
     test("Multiple resources output signatures") {
-        captureStandardOut {
-            cli.parse(listOf("-s", "src/test/resources/script", "src/test/resources/script2", "src/test/resources/view"))
-        } shouldBe """
+        val result =
+            captureStandardOut {
+                cli.parse(listOf(
+                    "-s",
+                    "-nr",
+                    "src/test/resources/script",
+                    "src/test/resources/script2",
+                    "src/test/resources/view"))
+            }.split("\n")
+                .map{x -> x.trim()}
+                .filter { x -> x.isNotBlank() }
+                .joinToString()
+
+        val reference = """
             7ea951abc0ddc97f549f41a5670b06aa513b30e189050159f40e207cfe502b02
             aa5f6ff86772d32ddad86da18914f835769ccd49e3603e8aea63f5b2fcaf7b08
             1f2e193ab0b2be15cef750b100bf5c6906b7a92fbb5e7c4f8fb7b68e83b4eb89
-        """.trimIndent() + "\n"
+        """.split("\n")
+            .map{x -> x.trim()}
+            .filter{ x -> x.isNotBlank()}
+            .joinToString()
+
+        //println("result\n$result")
+        //println("reference\n$reference")
+        result shouldBe reference
     }
 
     test("Single resource update") {
         captureStandardOut {
-            cli.parse(listOf("src/test/resources/script", "--actor", "test", "--timestamp", "2022-05-27T16:47:43Z"))
-        } shouldBe
-            javaClass.getResourceAsStream("/script2/resource.json").shouldNotBeNull().bufferedReader().readText() + "\n"
+            cli.parse(listOf(
+                "src/test/resources/script",
+                "--actor", "test",
+                "--timestamp", "2022-05-27T16:47:43Z",
+                "--console", "-nr"
+            )
+        )
+        }.trim() shouldBe
+            javaClass.getResourceAsStream(
+                "/script2/resource.json"
+            ).bufferedReader()
+                .readText()
+                .trim()
     }
 })
